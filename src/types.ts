6dbd2f5806674cab6aacb4deb69eb7f35dbc175b
KushaalNayak <kushaalbadavath8@gmail.com>
2026-02-03 14:30:00 +0530

    added core types and interfaces

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+
+export enum ProjectLanguage {
+    NodeJS = 'Node.js',
+    React = 'React',
+    Python = 'Python',
+    Java = 'Java',
+    CPP = 'C++'
+}
+
+export enum ProjectType {
+    WebApp = 'Web App',
+    CLITool = 'CLI Tool',
+    DSAPractice = 'DSA Practice',
+    MLProject = 'ML Project'
+}
+
+export interface ProjectConfig {
+    name: string;
+    language: ProjectLanguage;
+    type: ProjectType;
+    isTheRepoPublic: boolean; // Renamed to clearly indicate boolean flag
+    description: string;
+    path: string;
+}
